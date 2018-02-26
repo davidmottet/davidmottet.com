c8a172ad629acf981cf407368a06e73bdfdecdb5
David Mottet <david.mottet@tapat.org>
2018-02-26 14:18:51 +0100

    basic express

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express')
+const helmet = require('helmet')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
+
 const app = express()
+
+app.use(helmet())
+app.use(cookieParser())
+
+
+var sess = {
+	secret: 'davidmottet',
+	cookie: {}
+}
+
+if (app.get('env') === 'production') {
+	app.set('trust proxy', 1)
+	sess.cookie.secure = true
+}
 
 app.get('/', function (req, res) {
 	res.send('Hello World!')
