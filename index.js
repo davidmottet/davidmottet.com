4e8b365574fbd40d43d295daf860e74b13b8bc53
davidmottet <mottet.tlse@gmail.com>
2018-12-27 00:00:19 +0100

    update cert

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const express = require('express')
 const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
 const expressSession = require('express-session')
-const privateKey  = fs.readFileSync('sslcert/host.key', 'utf8')
-const certificate = fs.readFileSync('sslcert/host.crt', 'utf8')
+const privateKey  = fs.readFileSync('./sslcert/host.key', 'utf8')
+const certificate = fs.readFileSync('./sslcert/host.cert', 'utf8')
 
 const credentials = {key: privateKey, cert: certificate}
 
