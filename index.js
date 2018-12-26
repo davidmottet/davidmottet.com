625185b28f72290e1768bc90b9cb677aa6585532
davidmottet <mottet.tlse@gmail.com>
2018-12-26 23:34:52 +0100

    add FS

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
 const express = require('express')
 const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
 const expressSession = require('express-session')
-const privateKey  = fs.readFileSync('sslcert/host.key', 'utf8');
-const certificate = fs.readFileSync('sslcert/host.crt', 'utf8');
+const privateKey  = fs.readFileSync('sslcert/host.key', 'utf8')
+const certificate = fs.readFileSync('sslcert/host.crt', 'utf8')
 
-const credentials = {key: privateKey, cert: certificate};
-
-const https = require('https');
-const http = require('http');
+const credentials = {key: privateKey, cert: certificate}
 
 const app = express()
 const log = console.log
