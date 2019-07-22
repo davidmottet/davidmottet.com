acde5b22f1200e69de6fa28722769189a9a4c124
David MOTTET <contact@davidmottet.com>
2019-07-22 23:01:46 +0200

    [upd]

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 docker build -t davidmottet .
 
 docker stack deploy davidmottet --compose-file stack.yml
+
+
+openssl genrsa -des3 -out server.key 1024
+openssl req -new -key server.key -out server.csr
+//Création d'un backup
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
--- index.js
+++ index.js
@@ -2,22 +2,14 @@ const https = require('https');
 const http = require('http');
 const fs = require('fs');
 
-const express = require('express')
-const helmet = require('helmet')
-const cookieParser = require('cookie-parser')
-const expressSession = require('express-session')
-const privateKey  = fs.readFileSync('./sslcert/host.key', 'utf8')
-const certificate = fs.readFileSync('./sslcert/host.cert', 'utf8')
+const privateKey  = fs.readFileSync('./certs/server.key', 'utf8')
+const certificate = fs.readFileSync('./certs/server.crt', 'utf8')
 
 const credentials = {key: privateKey, cert: certificate}
 
-const app = express()
-const log = console.log
+const server = (req, res) => {
+	res.end('Hello !')
+}
 
-app.use(helmet())
-app.use(cookieParser())
-
-app.use(express.static('client'))
-
-http.createServer(app).listen(8080)
-https.createServer(credentials, app).listen(8443)
+http.createServer(server).listen(8080)
+https.createServer(credentials, server).listen(8443)
